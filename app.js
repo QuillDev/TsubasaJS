f13e7e236cb714748719408b62988c344cd8db07
Quill <siinspam@gmail.com>
2020-10-23 03:50:39 -0400

    added production mode

--- app.js
+++ app.js
@@ -4,11 +4,8 @@ const {Tsubasa} = require('./src/tsubasa/Tsubasa');
 //setup dotenv for local vars
 require('dotenv').config();
 
-
-let live = false;
-
 let config = {
-    token: live ? process.env.DISCORD_TOKEN_LIVE : process.env.DISCORD_TOKEN_TEST
+    token: (process.env.NODE_ENV === "PRODUCTION") ? process.env.DISCORD_TOKEN_LIVE : process.env.DISCORD_TOKEN_TEST
 }
 
 //create the client using the config
