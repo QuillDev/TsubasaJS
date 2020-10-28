ea1436d11c89b7843a3de9fa6160896dc0f10881
Quill <siinspam@gmail.com>
2020-10-28 01:27:07 -0400

    Modified ready handler to give more info about guilds

--- src/events/Ready.js
+++ src/events/Ready.js
@@ -11,7 +11,24 @@ class Ready extends TsubasaEvent {
     }
 
     async run() {
-        this.client.logger.debug(`${this.client.user.username}`, `Ready! Serving ${this.client.guilds.cache.size} guild(s) with ${this.client.users.cache.size} user(s)`);
+
+        //get the guilds cache
+        const guilds = this.client.guilds.cache;
+        //Log the names and ids of the guilds we're in
+        guilds.map(function(guild){
+            this.client.logger.debug(this.client.user.username, `Name: ${guild.name} |  ID: ${guild.id}`);
+        }, this);
+
+        //Log the amount of guild's we're in
+        this.client.logger.debug(`${this.client.user.username}`, `Ready! Serving ${guilds.size} guild(s) with ${this.client.users.cache.size} user(s)`);
+
+        //set the presence of the clinet
+        await this.client.user.setPresence({
+            status: "online",
+            activity: {
+                name: `Serving ${guilds.size} guilds!`
+            }
+        });
     }
 }
 module.exports = Ready;
