7000298bf99b2613dba5511f201a889eba5100bf
Robert Brunson <siinspam@gmail.com>
2021-03-09 01:21:24 -0500

    fuck youtube again

--- src/services/music/youtubeScraperService.js
+++ src/services/music/youtubeScraperService.js
@@ -26,7 +26,7 @@ async function search(query, page = 1) {
             }
 
             //try new formatting
-            let match = html.match(/ytInitialData[^{]*(.*?);\s*<\/script>/s);
+            let match = body.match(/ytInitialData[^{]*(.*?);\s*<\/script>/s);
 
             //if there were no matches use old formatting
             if (!match || match.length < 1) {
